d365493499a4131cddbe6a80d19b7446e45d3b2b
jcheung5 <jcheung5@villanova.edu>
2023-04-21 15:05:41 -0400

    added sample data for three tables

---
+++ backend/data.sql
@@ -0,0 +1,40 @@
+/* populating data for greenhouse schema */
+INSERT INTO LOCATION VALUES(seqOID.nextVal,'Desert Plants', '0', '90');
+INSERT INTO LOCATION VALUES(seqOID.nextVal,'Lawn Plants', '60', '70');
+INSERT INTO LOCATION VALUES(seqOID.nextVal,'Jungle Plants', '90', '80');
+INSERT INTO LOCATION VALUES(seqOID.nextVal,'Temperate Plants', '50', '60');
+INSERT INTO LOCATION VALUES(seqOID.nextVal,'Savannah Plants', '20', '70');
+
+INSERT INTO CUSTOMER VALUES 
+  ('Lucas','E','White','70844465','LWhite7', 'B4TbtxG');
+INSERT INTO CUSTOMER VALUES 
+  ('Bettie','T','Mals','60665443','BMals60','wIMr=Xe}');
+INSERT INTO CUSTOMER VALUES 
+  ('Mandy','S','Wallace','55079372','MWal55','S:`=slWi');
+INSERT INTO CUSTOMER VALUES
+  ('Jared','D','James','65465615','JJam65','m0WnZP\}');
+INSERT INTO CUSTOMER VALUES
+  ('Alex','J','Freeman','96251650','AFree96', 'WE;7uoP8');
+
+INSERT INTO EMPLOYEE VALUES 
+  ('James','E','Borg','888665555',4);
+INSERT INTO EMPLOYEE VALUES 
+  ('Sam','H','Moskowitz','333445555',2);
+INSERT INTO EMPLOYEE VALUES 
+  ('Alex','S','Wallace','987654321',5);
+INSERT INTO EMPLOYEE VALUES
+  ('Susan','D','James','111111100',3);
+INSERT INTO EMPLOYEE VALUES
+  ('Michael','B','Jordan', '444444400',2);
+INSERT INTO EMPLOYEE VALUES
+  ('John','C','James','555555500',1);
+  
+  
+
+    
+
+  
+  
+
+  
+  
